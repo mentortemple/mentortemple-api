51ca69fa4c3491b31cb2a563a0517319cada5efc
Kati Frantz <frantzenterprises777@gmail.com>
2018-07-26 05:34:40 +0100

    initial commit

--- admin/config/layout.js
+++ admin/config/layout.js
@@ -1,24 +1,24 @@
 module.exports = {
-  // contentTypesToShow: [
-  //   {
-  //     label: 'Users',
-  //     destination: 'user'
-  //   },
-  //   {
-  //     label: 'Courses',
-  //     destination: 'course'
-  //   },
-  //   {
-  //     label: 'Sections',
-  //     destination: 'section'
-  //   },
-  //   {
-  //     label: 'Lessons',
-  //     destination: 'lesson'
-  //   },
-  //   {
-  //     label: 'Enrollments',
-  //     destination: 'enrollment'
-  //   },
-  // ],
+  contentTypesToShow: [
+    {
+      label: 'Users',
+      destination: 'user'
+    },
+    {
+      label: 'Courses',
+      destination: 'course'
+    },
+    {
+      label: 'Sections',
+      destination: 'section'
+    },
+    {
+      label: 'Lessons',
+      destination: 'lesson'
+    },
+    {
+      label: 'Enrollments',
+      destination: 'enrollment'
+    },
+  ],
 };
